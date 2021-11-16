2aa4604a6996e0663fc37643f6921e770e853b61
heldgrind <ptitcorentin@live.fr>
2021-11-16 16:20:36 +0100

    test

--- TP3/src/grand_petit.c
+++ TP3/src/grand_petit.c
@@ -20,7 +20,7 @@ int max=0;
 int i=0;
  for (i = 0 ; i < sizeof(tabentier)/sizeof(*tabentier) ; i++)
     {
-    *(tabentier+i) = rand() % (100 + 1);            // remplissage du tableau
+    *(tabentier+i) = rand() % (800 + 1);            // remplissage du tableau
     printf("Entier :%d\n", *(tabentier+i));         // permet de tester la validé du code
     if (*(tabentier+i)>max)
        {
@@ -34,15 +34,8 @@ int i=0;
 
        }
     }
-      printf("Max :%d\n", *(tabentier+i));         // permet de tester la validé du code
-      printf("Min :%d\n", *(tabentier+i));         // permet de tester la validé du code
-/*
-      printf("rentrez un caractère ");
-    char caractere ='a';                 //initialisation du caractère
-    scanf("%c",&caractere);              //recuperation de la valeur rentrée par l'user et stockage à l'adresse de caractère
-    char *my_char_addr = &caractere;     // création du pointeur my_char_addr dont la valeur vaut l'adresse de la variable caractère
-    printf("char = %c\n", *my_char_addr);  // on affiche la valeur contenu dans my_char_addr grâce à un pointeur
-    printf("adresse char = %p\n", my_char_addr); // on affiche l'adresse de la valeur*/
+      printf("Max :%d\n", max);         // permet de tester la validé du code
+      printf("Min :%d\n", min);         // permet de tester la validé du code
 return 0;
 }
 
